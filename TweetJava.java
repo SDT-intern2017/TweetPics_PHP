1412a8b1cc8a5900a680f9298e3a30473b184a01
SDT-intern2017 <sdtintern2017@gmail.com>
2017-07-19 17:35:20 +0400

    Add files via upload

---
+++ TweetJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tweetjava;
+
+/**
+ *
+ * @author Ramya
+ */
+public class TweetJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Status status = new Status();
+        status.setVisible(true);
+    }
+    
+}
