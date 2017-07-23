feee589c7b301fa884221a81be7b230637dbebba
SDT-intern2017 <sdtintern2017@gmail.com>
2017-07-23 18:33:27 +0400

    Add files via upload
    Compare.form has the form to input images
    Compare.java checks if the images are same

---
+++ ImgProcess.java
@@ -0,0 +1,17 @@
+/*
+ * To compare images
+ * @author Ramya
+ */
+package imgprocess;
+/**
+ *
+ * @author Ramya
+ */
+
+public class ImgProcess {
+
+ public static void main(String[] args) {
+     Compare compare = new Compare();
+     compare.setVisible(true);
+ }
+}
